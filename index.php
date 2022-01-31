15d6bfc9083ac2d685abd88b6068cef1cf5963ca
kabarkredit <80444401+kabarkredit@users.noreply.github.com>
2022-02-01 01:09:21 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
-$url='http://finance.detik.com/read/2014/09/24/104509/2699424/4/mampu-bikin-pesawat-terbang-pekerja-ri-diklaim-lebih-unggul-di-asean?f9911013';
+$url='https://www.maskoding.com/2021/02/redirect-url-berdasarkan-sumber-trafik.html';
 $data=file_get_contents($url);
 echo $data;
 ?>
